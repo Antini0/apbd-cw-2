b797f2f912b282da0d47d25cc784aabcdbacbf7c
Antoni Dawidowicz <s27366@pjwstk.edu.pl>
2024-03-12 11:28:59 +0100

    MOD AVG (4)

--- AntoniApp1/AntoniApp1/Program.cs
+++ AntoniApp1/AntoniApp1/Program.cs
@@ -4,3 +4,16 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("MOD 1");
 Console.WriteLine("MOD 2");
 Console.WriteLine("MOD 3");
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var num in numbers)
+    {
+        sum += num;
+    }
+
+    sum = sum / numbers.Length;
+    return (double)sum;
+
+}
